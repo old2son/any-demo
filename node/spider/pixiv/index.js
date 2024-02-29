fb36341aef2982bbea0d3d5d4b0197bcd45c5593
CJY <13632362832@163.com>
2024-02-29 19:39:18 +0800

    test pixiv rank

--- node/spider/pixiv/index.js
+++ node/spider/pixiv/index.js
@@ -10,13 +10,14 @@ const proxyAgent = new SocksProxyAgent(process.env.SOCKET_PROXY);
 // 创建 HTTP 请求选项
 const requestOptions = {
 	hostname: 'www.pixiv.net',
-	path: '/artworks/108212190', // 你要请求的路径
+	// path: '/artworks/108212190', // 你要请求的路径
+	path: '/ranking.php?mode=daily_r18', // 你要请求的路径
 	method: 'GET',
 	headers: {
 		'Accept-Charset': 'UTF-8',
 		'User-Agent': process.env.USER_AGENT,
 		Cookie: process.env.PIXIV_COOKIE,
-		Referer: 'https://www.pixiv.net/artworks/108212190'
+		Referer: 'https://www.pixiv.net/ranking.php'
 	},
 	agent: proxyAgent // 使用代理
 };
@@ -32,10 +33,11 @@ const request = https.request(requestOptions, (response) => {
 
 	// 数据接收完成
 	response.on('end', () => {
+		console.log(html)
 		const $ = cheerio.load(html);
-		const text = JSON.parse($('#meta-preload-data').attr('content'));
+		/* const text = JSON.parse($('#meta-preload-data').attr('content'));
 		const img = text.illust['108212190'].urls.original;
-		downloadImage(img);
+		downloadImage(img); */
 	});
 });
 
